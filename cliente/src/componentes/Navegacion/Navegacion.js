1797ee1a22c12117538a9921e5c951cdf6ed69af
cristian <cristian982940@gmail.com>
2019-06-13 15:06:45 -0500

    add button sign in y logout

--- cliente/src/componentes/Navegacion/Navegacion.js
+++ cliente/src/componentes/Navegacion/Navegacion.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navegacion.css';
 
-const Navegacion = () => {
-     return ( 
-          <nav className="navegacion">
-               <NavLink to={'/nosotros'} activeClassName="activo">Nosotros</NavLink>
-               <NavLink to={'/productos'} activeClassName="activo">Productos</NavLink>
-               <NavLink to={'/contacto'} activeClassName="activo">Contacto</NavLink>
-          </nav>
-      );
+class Navegacion extends Component{
+
+  cerrarSesion = ( ) =>{
+    this.props.auth.logout();
+  }
+
+  iniciarSesion = () =>{
+    this.props.auth.login()
+  }
+  render(){    
+    const { isAuthenticated } = this.props.auth;
+    let resultado;
+    if( isAuthenticated() ){
+      resultado = <a onClick={this.cerrarSesion}>Cerrar Session</a>
+    }else{
+      resultado = <a onClick={this.iniciarSesion}>Iniciar Sesion</a>
+    }
+
+
+    return ( 
+      <nav className="navegacion">
+        <NavLink to={'/nosotros'} activeClassName="activo">Nosotros</NavLink>
+        <NavLink to={'/productos'} activeClassName="activo">Productos</NavLink>
+        <NavLink to={'/contacto'} activeClassName="activo">Contacto</NavLink>
+        {resultado}
+      </nav>
+    );
+  }
 }
  
 export default Navegacion;
--- cliente/src/componentes/routes.js
+++ cliente/src/componentes/routes.js
@@ -27,7 +27,7 @@ export const makeMainRoutes = () => {
       <Router history={history}>
         <div className="contenedor">
             <Header />
-            <Navegacion />
+            <Navegacion auth={auth}/>
 
           <Route exact path="/" render={(props) => (
             <Productos
